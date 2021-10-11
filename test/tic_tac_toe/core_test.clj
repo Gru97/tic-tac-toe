02e60bcfaec1cc6a5cc2ddda54c9bc37b28c1f57
gru97 <fatemeh.shahidani@gmail.com>
2021-10-11 22:42:41 +0330

    end game scenarios

--- test/tic_tac_toe/core_test.clj
+++ test/tic_tac_toe/core_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [tic-tac-toe.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def tripple '((1 2 3) (4 5 6) (7 8 9) (1 4 7) (2 5 8) (3 6 9) (1 5 9) (3 5 7)))
+
+(deftest create-tripples
+  (testing "tripples must be created"
+    (is (= tripple (tripples [1 2 3 4 5 6 7 8 9])))))
+
+
