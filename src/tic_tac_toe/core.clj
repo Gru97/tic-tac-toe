ab555485d4a4e0aca0112b3dad4ad7a21ab5ce08
gru97 <fatemeh.shahidani@gmail.com>
2021-10-14 22:25:00 +0330

    game completed

--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,6 +1,64 @@
 (ns tic-tac-toe.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def starting-board [1 2 3 4 5 6 7 8 9])
+
+(defn tripples [board]
+  (concat (partition-all 3 board)
+          (list
+           (take-nth 3 board)
+           (take-nth 3 (drop 1 board))
+           (take-nth 3 (drop 2 board))
+           (take-nth 4 board)
+           (take-nth 2 (drop-last 2 (drop 2 board))))))
+
+;(tripples board)
+
+(defn full-board [board]
+  (every? #{:x :o} board))
+
+
+(defn tripple-winner [tripple]
+  (if (every? #{:x} tripple) :x (if (every? #{:o} tripple) :o nil)))
+
+(defn who-won [board]
+  (first (filter #{:x :o} (map tripple-winner (tripples board)))))
+
+
+(filter #{:x} [:x :x :y])
+;; => (:x :x)
+
+; finit number of choise for a tic-tac-toe: 9 sequence
+(def game-rounds (cycle #{:o :x}))
+(take 9 game-rounds)
+
+(defn get-move-from-player [board]
+        (let [input
+                (try (. Integer parseInt (read-line))
+                  (catch Exception e nil))]
+        (if (some #{input} board) input nil)))
+
+(defn change-board [board move player]
+  (assoc board (dec move) player))
+
+(change-board [1 2 3 4] 1 9)
+
+(defn display-board [board] (println (partition-all 3 board)))
+
+ (defn take-turn [board rounds]
+   (let [player (first rounds)]
+     (println (str "player: " (name player)))
+     (change-board board (get-move-from-player board) player) ))
+
+(defn play-game [starting-board game-rounds]
+  (loop [starting-board starting-board game-rounds game-rounds]
+    (println "current board:") (display-board starting-board)
+    (let [winner (who-won starting-board)]
+     (cond
+        winner (print (str (name winner) " won!"))
+       (full-board starting-board) (println "game is drawn")
+       :else
+          (recur (take-turn starting-board game-rounds) (rest game-rounds))))))
+
+(play-game starting-board game-rounds)
+
+
--- test/tic_tac_toe/core_test.clj
+++ test/tic_tac_toe/core_test.clj
@@ -4,8 +4,34 @@
 
 (def tripple '((1 2 3) (4 5 6) (7 8 9) (1 4 7) (2 5 8) (3 6 9) (1 5 9) (3 5 7)))
 
-(deftest create-tripples
+(deftest create-tripples-test
   (testing "tripples must be created"
     (is (= tripple (tripples [1 2 3 4 5 6 7 8 9])))))
 
+(deftest full-board-test
+  (testing "should return true if board is full"
+    (is (= true (full-board [:x :o :x :o :x])))
+    (is (= false (full-board [1 2 :x 4])))))
 
+
+(deftest tripple-winner-test
+  (testing "should return winner if one player is won"
+    (is (= :x (tripple-winner  [:x :x :x ])))
+    (is (= nil (tripple-winner  [:x 1 :x])))
+    (is (= :o (tripple-winner  [:o :o :o])))))
+
+(deftest who-won-test
+  (testing "should return winner of the board"
+    (is (= :x (who-won  [:x :x :x 4 5 6 7 8 9])))
+    (is (= :o (who-won  [1 :o 3 4 :o 6 7 :o 9])))
+    (is (= nil (who-won  [1 2 :o 4 :o 6 7 :o 9])))))
+
+
+(deftest get-move-from-player-test
+  (testing "should accept a number"
+    (is (= 1 (get-move-from-player [1 2 3 4 5 6 7 8 9])))))
+
+(deftest play-game-test
+  (testing "should-start-game"
+    (play-game [1 2 3 4] [:x :y :x :y])))
+(run-tests)
